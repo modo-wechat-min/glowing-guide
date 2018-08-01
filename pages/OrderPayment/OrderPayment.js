132d6e9b578baa485ad5bdd638a7065fc60ca70b
mengchun <mengchun@ttwu.net>
2018-08-01 16:21:09 +0800

    孟春：8.1

--- pages/OrderPayment/OrderPayment.js
+++ pages/OrderPayment/OrderPayment.js
@@ -1,66 +1,37 @@
 // pages/OrderPayment/OrderPayment.js
+let leftTime = 1000 * 60 * 15;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    time:"",
+    timeout:true,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    var _this=this;
+    setInterval(function () { _this.leftTimer() }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  leftTimer() {
+    // let leftTime = (new Date(enddate)) - new Date(); //计算剩余的毫秒数
+    let days = parseInt(leftTime / 1000 / 60 / 60 / 24,10); //计算剩余的天数
+    let hours = parseInt(leftTime / 1000 / 60 / 60 % 24, 10);//计算剩余的小时
+    let minutes = parseInt(leftTime / 1000 / 60 % 60, 10); //计算剩余的分钟
+    let seconds = parseInt(leftTime / 1000 % 60, 10); //计算剩余的秒数
+    minutes = this.checkTime(minutes);
+    seconds = this.checkTime(seconds);
+    if (days >= 0 || hours >= 0 || minutes >= 0 || seconds >= 0) {
+      this.setData({
+        time: minutes + ":" + seconds ,
+      })
+    }
+    if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+      window.clearInterval(_ordertimer);
+      _ordertimer = null;
+    };
+    leftTime = leftTime-1000;
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  checkTime(i) { //将0-9的数字前面加上0，例1变为01
+    if (i < 10) {  
+      i = "0" + i;  
+    }  
+    return i; 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
--- pages/success/success.js
+++ pages/success/success.js
@@ -1,6 +1,6 @@
 Page({
   data:{
-    type: "clean",//type类型对应不同状态，buy购买会员卡，clean预约打扫，charge充值，comment评价
+    type: "clean",//type类型对应不同状态，buy购买会员卡，clean预约打扫，charge充值，comment评价,payment订单支付成功
   },
    onLoad: function (options) {
      this.setData({
