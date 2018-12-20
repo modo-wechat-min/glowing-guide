355d850ce391a5fd97c9c947bc01adc3aa67ad06
mengchun <mengchun@ttwu.net>
2018-12-20 21:45:07 +0800

    孟春

--- utils/ports.js
+++ utils/ports.js
@@ -7,23 +7,23 @@
 // let modoHttp2 = "http://192.168.1.180:8008"; 
 //线上站点
 let imgUrl ="https://www.modo-life.com/Images/WeChatMiniStatic/"  
-// let modoHttp = "https://www.modo-life.com/"
-// let modoHttp2 = "https://www.modo-life.com"    
+let modoHttp = "https://www.modo-life.com/"
+let modoHttp2 = "https://www.modo-life.com"    
 
 // let modoHttp = "https://www.modo-apt.cn/"
 // let modoHttp2 = "https://www.modo-apt.cn" 
 
 // 李淑华站点 
 // let modoHttp = "http://192.168.1.185:5067/"  
-// let modoHttp2 = "http://192.168.1.185.5067"        
+// let modoHttp2 = "http://192.168.1.185.5067"         
  
 // let modoHttp = "http://app.modo38.com/"  
-// let modoHttp2 = "http://app.modo38.com"     
+// let modoHttp2 = "http://app.modo38.com"      
  
 
 // 张泽宇
-let modoHttp = "http://192.168.1.187:8082/"
-let modoHttp2 = "http://192.168.1.187:8082"  
+// let modoHttp = "http://192.168.1.187:8082/"
+// let modoHttp2 = "http://192.168.1.187:8082"  
  
 let modoImgHttp = "https://www.52modo.com";//线上图片接口
 module.exports = {
