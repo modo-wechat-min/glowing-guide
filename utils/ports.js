62968841634161e61b1deed4da37c31f8c60ab6a
mengchun <mengchun@ttwu.net>
2019-01-10 10:15:32 +0800

    孟春

--- utils/ports.js
+++ utils/ports.js
@@ -3,8 +3,8 @@
 // let modoHttp = "https://testapp.modo-life.com/"
 // let modoHttp2 = "https://testapp.modo-life.com"
 //晨野站点 
-let modoHttp ="http://192.168.1.180:8008/";
-let modoHttp2 = "http://192.168.1.180:8008"; 
+// let modoHttp ="http://192.168.1.180:8008/";
+// let modoHttp2 = "http://192.168.1.180:8008"; 
 //线上站点
 let imgUrl ="https://www.modo-life.com/Images/WeChatMiniStatic/"   
 // let modoHttp = "https://www.modo-life.com/"
@@ -18,8 +18,8 @@ let imgUrl ="https://www.modo-life.com/Images/WeChatMiniStatic/"
 
 // let modoHttp2 = "http://192.168.1.185.5067"         
  
-// let modoHttp = "http://app.modo38.com/"  
-// let modoHttp2 = "http://app.modo38.com"       
+let modoHttp = "http://app.modo38.com/"  
+let modoHttp2 = "http://app.modo38.com"        
  
 //高帅
 // let modoHttp = "http://192.168.1.95:8081/"
